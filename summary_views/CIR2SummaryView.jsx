590074b033b597c6b4852f25fa2662e0330bb72f
Tony Yuen <colsy2@gmail.com>
2019-04-18 20:12:35 +0800

    optimise CIR2 summery view

--- summary_views/CIR2SummaryView.jsx
+++ summary_views/CIR2SummaryView.jsx
@@ -1,16 +1,75 @@
 
 import SingleCCSummaryView from './SingleCCSummaryView'
+import {
+	Tag,
+	Timeline,
+	Card,
+	Statistic, 
+	Row, 
+	Col, 
+	Icon,
+	Table,
+	Divider
+} from 'antd'
 
 export default class CIR2SummaryView extends SingleCCSummaryView {
-	getPlanname() {
-		return "CIR2"
-	}
 
-	getDiseaseNum() {
-		return 75
-	}
+	renderHighlight() {
 
-	getClaimNum() {
-		return 1;
+		const {
+			totalFreeInsuredYears,
+			basicInsured,
+			freeInsured
+		} = this.props.summary;
+
+		const firstYearAssured = basicInsured + freeInsured;
+
+		return (
+			<div>
+			<Tag color="blue">严重病况单次理赔方案:</Tag>
+			<div style={{"margin-top": "10px"}}>
+					
+			<Row gutter={16}>
+				    <Col span={8}>
+				      <Statistic 
+				      	title={"起始保额(" + this.toCurrencyFormat(firstYearAssured).split(" ")[1] + ")"} 
+				      	value={this.toCurrencyFormat(firstYearAssured).split(" ")[0]}
+				      	valueStyle={{
+				      		"color": "rgb(154, 29, 73)",
+    						"font-size": "30px"
+				      	}}
+				    />
+				    </Col>
+				    <Col span={8}>
+				      <Statistic 
+				      	title={"涵盖病种"} 
+				      	value={75}
+				      	valueStyle={{
+				      		"color": "rgb(154, 29, 73)",
+    						"font-size": "30px"
+				      	}}
+				      />
+				    </Col>
+				    <Col span={8}>
+				      <Statistic 
+				      	title="末期病况，额外理赔"
+				      	value={20}
+				      	prefix={<Icon type="arrow-up" />}
+				      	precision={2}
+            			suffix="%"
+				      	valueStyle={{
+				      		"color": '#3f8600',
+    						"font-size": "30px"
+				      	}}
+				      />
+				    </Col>
+			  </Row>
+			</div>
+			  <Divider />
+			  <p style={{textAlign: 'center', marginTop: "-20px"}}>
+			  	基本重疾保额{this.toCurrencyFormat(basicInsured)} | 前{totalFreeInsuredYears}赠送的重疾保额{this.toCurrencyFormat(freeInsured)}
+			  </p>				  
+			</div>
+		)
 	}
 }
